a4d4164f3105fcc3a44eaa91906d258312519952
Chetan <pop.chetan@gmail.com>
2015-01-22 14:18:41 +0530

    GCM working

--- app/src/main/java/com/api_hackathon/app/broadcast/GcmBroadcastReceiver.java
+++ app/src/main/java/com/api_hackathon/app/broadcast/GcmBroadcastReceiver.java
@@ -5,6 +5,7 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.content.WakefulBroadcastReceiver;
+import android.util.Log;
 import com.api_hackathon.app.service.GcmIntentService;
 
 /**
@@ -14,6 +15,7 @@ public class GcmBroadcastReceiver extends WakefulBroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        Log.e("chetan", "BroadcastReceiver");
         ComponentName comp = new ComponentName(context.getPackageName(),
                 GcmIntentService.class.getName());
         startWakefulService(context, (intent.setComponent(comp)));
--- app/src/main/java/com/api_hackathon/app/service/GcmIntentService.java
+++ app/src/main/java/com/api_hackathon/app/service/GcmIntentService.java
@@ -5,8 +5,10 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.provider.Settings;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import com.api_hackathon.app.R;
@@ -28,10 +30,11 @@ public class GcmIntentService  extends IntentService {
         super("GcmIntentService");
     }
 
-    public static final String TAG = "GCMNotificationIntentService";
+    public static final String TAG = "chetan";
 
     @Override
     protected void onHandleIntent(Intent intent) {
+        Log.e(TAG,"onHandleIntent");
         Bundle extras = intent.getExtras();
         GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
 
@@ -66,12 +69,14 @@ public class GcmIntentService  extends IntentService {
                 new Intent(this, MainActivity.class), 0);
 
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(
-                this).setSmallIcon(R.drawable.search_icon)
+                this).setSmallIcon(R.drawable.tick)
                 .setContentTitle("GCM Notification")
                 .setStyle(new NotificationCompat.BigTextStyle().bigText(msg))
                 .setContentText(msg);
 
         mBuilder.setContentIntent(contentIntent);
+        mBuilder.setSound(Settings.System.DEFAULT_NOTIFICATION_URI);
+        mBuilder.setLights(Color.RED, 3000, 3000);
         mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
         Log.d(TAG, "Notification sent successfully.");
     }
